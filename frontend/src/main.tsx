7cb381d9ba36a0d8f6f728fec48810a4d8b3e262
astercc518 <astercc518@users.noreply.github.com>
2026-06-05 10:38:53 +0000

    fix(antd): wrap root in AntD App so modal.confirm hooks work in production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { App as AntdApp } from 'antd'
 import { queryClient } from './lib/queryClient'
 import App from './App'
 import './index.css'
@@ -12,7 +13,9 @@ if (!root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AntdApp>
+          <App />
+        </AntdApp>
       </QueryClientProvider>
     </React.StrictMode>,
   )
